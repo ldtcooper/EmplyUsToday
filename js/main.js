f8182ca710fbee7ea0656ed7dec5f09ae182b483
Harsh <harsh@hkumar.me>
2017-11-29 15:57:28 -0800

    Add css and update js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,26 @@
 document.addEventListener("DOMContentLoaded",() => {
   console.log("DOCUMENT READY", engineers);
   const engineersContainer = document.getElementById('engineers')
+
+  totalEngineers = Object.keys(engineers).length
+  const engineersCounter = document.getElementById('counter')
+  engineersCounter.innerHTML = totalEngineers
+
   Object.keys(engineers).forEach(key => {
     const engineer = engineers[key]
     const listItem = document.createElement('li')
     listItem.id = key
+    listItem.className = 'engineer'
+    listItem.appendChild(createImage(engineer.github, engineer.fullName, engineer.fullName))
     listItem.appendChild(createHeader(engineer.fullName))
     listItem.appendChild(createGithubLink(engineer.github))
     listItem.appendChild(createLinkedinLink(engineer.linkedin))
     listItem.appendChild(createLink(engineer.portfolio, 'Portfolio'))
-    listItem.appendChild(createImage(engineer.imageUrl, engineer.fullName, engineer.fullName))
+    listItem.addEventListener('click', e => toggleFull(e))
     engineersContainer.appendChild(listItem)
   })
+
+
 })
 
 const engineers = {
@@ -20,7 +29,7 @@ const engineers = {
     github: 'hkumar1993',
     linkedin: 'hkumar1993',
     portfolio: 'http://www.hkumar.me',
-    imageUrl: '#',
+    imageUrl: 'images/harsh.jpg',
     skills: [
       'Ruby',
       'Ruby on Rails',
@@ -45,18 +54,36 @@ const engineers = {
     linkedin: 'woodentyler',
     portfolio: 'http://woodentyler.com',
     imageUrl: '#'
-  }
+  },
+  tyler1: {
+    fullName: 'Tyler Wood',
+    github: 'woodentyler',
+    linkedin: 'woodentyler',
+    portfolio: 'http://woodentyler.com',
+    imageUrl: '#'
+  },
+  tyler2: {
+    fullName: 'Tyler Wood',
+    github: 'woodentyler',
+    linkedin: 'woodentyler',
+    portfolio: 'http://woodentyler.com',
+    imageUrl: '#'
+  },
+
 }
 
 // source for backwards compatible image creation:
 // https://stackoverflow.com/questions/226847/what-is-the-best-javascript-code-to-create-an-img-element
 
 const createImage = (src, alt, title) => {
+    const imgContainer = document.createElement('div')
+    imgContainer.className = 'profilePicture'
     const img = document.createElement('img')
-    if (src != null ) img.src = src
+    if (src != null ) img.src = `https://avatars.githubusercontent.com/${src}`
     if ( alt != null ) img.alt = `Image of ${alt}`
     if ( title != null ) img.title = title
-    return img
+    imgContainer.appendChild(img)
+    return imgContainer
 }
 
 const createLink = (link, title) => {
@@ -78,4 +105,15 @@ const createHeader = (name) => {
   const heading = document.createElement('h1')
   heading.innerHTML = name
   return heading
+}
+
+const toggleFull = (e) => {
+  const engineers = Array.from(document.getElementsByClassName('engineer full'))
+  console.log(engineers);
+  engineers.forEach(engineer => engineer.className = 'engineer')
+  if(e.target.localName === 'img'){
+    e.target.parentElement.parentElement.className = 'engineer full'
+  } else if (e.target.localName === 'li'){
+    e.target.className = 'engineer full'
+  }
 }
